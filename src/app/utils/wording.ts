ac7e55116eeb8aa8be42da38e416e444650fabdf
Popysid <popysid@gmail.com>
2023-04-15 08:27:27 -0400

    added central wording constant to have the text in one place

---
+++ src/app/utils/wording.ts
@@ -0,0 +1,16 @@
+export const wording = {
+    title: "Olympic Games",
+    page: {
+        home: {
+            subtitle: "General informations",
+            description: "Télésport Web Application that displays data about the last Olympic Games.",
+        },
+        details: {
+            subtitle: "Details per country"
+        },
+        notFound: {
+            subtitle: "404: Page not found"
+        }
+
+    }
+}
